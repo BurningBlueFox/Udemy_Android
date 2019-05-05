6ba10e8da36eb62f05964aa28d7ec6e6a8ead0e8
Thiago Gorgulho Leão <burningbluefox@gmail.com>
2019-05-05 02:22:39 -0300

    Basic button Interactivity

--- 02ProjectButton/app/src/main/java/com/thiago/mybutton/MainActivity.java
+++ 02ProjectButton/app/src/main/java/com/thiago/mybutton/MainActivity.java
@@ -1,13 +1,29 @@
 package com.thiago.mybutton;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bt = findViewById(R.id.btn_heyAndroid);
+        bt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("Test", "Button Clicked!");
+                bt.setText(R.string.bt_2);
+            }
+        });
     }
 }

8ccd27e796b821aea06652962a387706fb856e66
Thiago <thiagogorgulho@outlook.com>
2019-05-07 02:00:13 -0300

    Makes linear layout as a toast message

--- 03ProjectToast/app/src/main/java/com/thiago/customtoast/MainActivity.java
+++ 03ProjectToast/app/src/main/java/com/thiago/customtoast/MainActivity.java
@@ -2,7 +2,11 @@ package com.thiago.customtoast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +18,23 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void ButtonRedClicked(View view){
-        Toast.makeText(this, "OK", Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this, "OK", Toast.LENGTH_SHORT).show();
+        ShowCustomToast("Hello!");
+    }
+
+    public void ShowCustomToast(String string){
+        LayoutInflater inflater = getLayoutInflater();
+        View layout = inflater.inflate(R.layout.toast_layout, (ViewGroup)findViewById(R.id.toast_root));
+
+        TextView textView = layout.findViewById(R.id.toast_text);
+        textView.setText(string);
+
+        Toast toast = new Toast(getApplicationContext());
+
+        toast.setGravity(Gravity.BOTTOM, 0, 10);
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setView(layout);
+
+        toast.show();
     }
 }

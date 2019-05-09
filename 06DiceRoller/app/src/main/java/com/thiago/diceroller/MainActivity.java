18cae7c15c77fa97a073b631945d231a5614ebf8
Thiago <thiagogorgulho@outlook.com>
2019-05-09 13:08:08 -0300

    Updates app icon and functionality

--- 06DiceRoller/app/src/main/java/com/thiago/diceroller/MainActivity.java
+++ 06DiceRoller/app/src/main/java/com/thiago/diceroller/MainActivity.java
@@ -2,6 +2,10 @@ package com.thiago.diceroller;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void OnClickRollDice(View view){
+        RollDice();
+    }
+
+    public void RollDice(){
+        Random random = new Random();
+        int randomNumber = random.nextInt(6) + 1;
+
+        ImageView image = (ImageView)findViewById(R.id.image_dice);
+
+        switch (randomNumber){
+            case 1: image.setImageResource(R.drawable.dice_1);
+                break;
+            case 2: image.setImageResource(R.drawable.dice_2);
+                break;
+            case 3: image.setImageResource(R.drawable.dice_3);
+                break;
+            case 4: image.setImageResource(R.drawable.dice_4);
+                break;
+            case 5: image.setImageResource(R.drawable.dice_5);
+                break;
+            case 6: image.setImageResource(R.drawable.dice_6);
+                break;
+        }
     }
 }

bf7d9ac53839a34fb10d704449a1892eb97ab2eb
Thiago <thiagogorgulho@outlook.com>
2019-05-10 15:34:35 -0300

    Creates app 7

--- 06DiceRoller/app/src/main/java/com/thiago/diceroller/MainActivity.java
+++ 06DiceRoller/app/src/main/java/com/thiago/diceroller/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import java.util.Random;
 
@@ -19,6 +20,11 @@ public class MainActivity extends AppCompatActivity {
         RollDice();
     }
 
+//    public void AddDice(){
+//        LinearLayout linearLayout  = (LinearLayout) findViewById(R.id.layout_dice);
+//
+//        linearLayout
+//    }
     public void RollDice(){
         Random random = new Random();
         int randomNumber = random.nextInt(6) + 1;
